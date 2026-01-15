f3f7d55564ceef35a3a0dbe8c2b300dfb9876315
vanntl <vanntl@vng.com.vn>
2026-01-15 16:26:38 +0700

    chore: bump version to 1.0.1

--- src/manifest.ts
+++ src/manifest.ts
@@ -3,7 +3,7 @@ import { defineManifest } from '@crxjs/vite-plugin'
 export default defineManifest({
   manifest_version: 3,
   name: 'Vocabulary Builder',
-  version: '1.0.0',
+  version: '1.0.1',
   description: 'Learn vocabulary with flashcards, spaced repetition, and context menu lookup',
 
   permissions: [
